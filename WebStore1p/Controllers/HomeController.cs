d11df392ba0a588097a2b08e48ace5269a8434d3
olprog8 <solar2010@live.ru>
2020-06-15 20:18:07 +0300

    Пример работы с моделью предметной области

--- WebStore1p/Controllers/HomeController.cs
+++ WebStore1p/Controllers/HomeController.cs
@@ -1,10 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using WebStore1p.Models;
 
 namespace WebStore1p.Controllers
 {
     public class HomeController : Controller
     {
+
+        private static readonly List<Employee> __Employees = new List<Employee>()
+        {
+            new Employee
+            {
+                Id =1,
+                SurName = "Иванов",
+                FirstName = "Иван",
+                Patronymic = "Иванович",
+                Age = 39
+            }
+            ,
+            new Employee
+            {
+                Id =2,
+                SurName = "Петров",
+                FirstName = "Петр",
+                Patronymic = "Петрович",
+                Age = 18
+            },
+            new Employee
+            {
+                Id =3,
+                SurName = "Сидоров",
+                FirstName = "Сидор",
+                Patronymic = "Сидорович",
+                Age = 27
+            }
+        };
+
         //public async Task<IActionResult> Index()
         public IActionResult Index()
         {
@@ -16,5 +49,17 @@ namespace WebStore1p.Controllers
             return View();
         }
 
+        public IActionResult Employees()
+        {
+            return View(__Employees);
+        }
+
+        public IActionResult Employee(int Id)
+        {
+            var employee = __Employees.FirstOrDefault(e => e.Id == Id);
+            if (employee is null)
+                return NotFound();
+            return View(employee);
+        }
     }
 }
