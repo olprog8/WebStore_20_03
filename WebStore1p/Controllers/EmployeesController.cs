280461d2187b345237667d2cb4c9fcc93c565d6a
olprog8 <solar2010@live.ru>
2020-06-29 18:39:12 +0300

    Перенесли Employees в отдельный класс и в папку TestData

--- WebStore1p/Controllers/EmployeesController.cs
+++ WebStore1p/Controllers/EmployeesController.cs
@@ -1,55 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-
-using WebStore1p.Models;
+using WebStore1p.Data;
 
 namespace WebStore1p.Controllers
 {
 
-    [Route("users")]
+    //[Route("users")]
     public class EmployeesController : Controller
     {
 
-        private static readonly List<Employee> __Employees = new List<Employee>()
-        {
-            new Employee
-            {
-                Id =1,
-                SurName = "Иванов",
-                FirstName = "Иван",
-                Patronymic = "Иванович",
-                Age = 39
-            }
-            ,
-            new Employee
-            {
-                Id =2,
-                SurName = "Петров",
-                FirstName = "Петр",
-                Patronymic = "Петрович",
-                Age = 18
-            },
-            new Employee
-            {
-                Id =3,
-                SurName = "Сидоров",
-                FirstName = "Сидор",
-                Patronymic = "Сидорович",
-                Age = 27
-            }
-        };
-
-
-        [Route("Employees")]
-        public IActionResult Index() => View(__Employees);
+        //[Route("Employees")]
+        public IActionResult Index() => View(TestData.Employees);
         
-        [Route("Employee/{Id}")]
+        //[Route("Employee/{Id}")]
         public IActionResult Details(int Id)
         {
-            var employee = __Employees.FirstOrDefault(e => e.Id == Id);
+            var employee = TestData.Employees.FirstOrDefault(e => e.Id == Id);
             if (employee is null)
                 return NotFound();
             return View(employee);
