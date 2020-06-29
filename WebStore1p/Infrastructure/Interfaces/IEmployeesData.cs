9e3c57b57b928c6e48c03bc38daf6a2b0f01437f
olprog8 <solar2010@live.ru>
2020-06-29 18:49:35 +0300

    Создали интерфейс IEmployeesData

---
+++ WebStore1p/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore1p.Models;
+
+namespace WebStore1p.Infrastructure.Interfaces
+{
+    //Создаем сервис (в виде интерфейса и перечисляем его члены)
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> GetAll();
+
+        Employee GetById(int id);
+
+        void Add(Employee Employee);
+
+        void Edit(int id, Employee Employee);
+
+        bool Delete(int id);
+
+        void SaveChages();
+    }
+}
