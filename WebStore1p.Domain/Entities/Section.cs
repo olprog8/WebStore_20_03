f354e98c208ea61c31d4cd01566b29186676a308
olprog8 <solar2010@live.ru>
2020-07-23 13:26:21 +0300

    Бренд и секция - первые доменные модели

---
+++ WebStore1p.Domain/Entities/Section.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore1p.Domain.Entities.Base;
+using WebStore1p.Domain.Entities.Base.Interfaces;
+
+namespace WebStore1p.Domain.Entities
+{
+    /// <summary>Секция товаров</summary>
+    public class Section: NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+       /// <summary>Идентификатор родительской секции</summary>
+        public int? ParentId { get; set; }
+    }
+}
