db616a297f32cce747333ab15a1160b0f0af81d7
olprog8 <solar2010@live.ru>
2020-10-06 19:27:24 +0300

    Добавление ProductMapping

---
+++ WebStore1p/Infrastructure/Mapping/ProductMapping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using WebStore1p.ViewModels;
+using WebStore1p.Domain.Entities;
+
+namespace WebStore1p.Infrastructure.Mapping
+{
+    public static class ProductMapping
+    {
+        public static ProductViewModel ToView(this Product p) => new ProductViewModel
+        {
+            Id = p.Id,
+            Name = p.Name,
+            Order = p.Order,
+            Price = p.Price,
+            ImageUrl = p.ImageUrl
+        };
+    }
+}
