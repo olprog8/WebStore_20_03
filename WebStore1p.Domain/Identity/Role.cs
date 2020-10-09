510779da8fb362eb99ffd7f69f75f072908eddc0
olprog8 <solar2010@live.ru>
2020-10-09 18:08:58 +0300

    Сущности Пользователь и Роль

---
+++ WebStore1p.Domain/Identity/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace WebStore1p.Domain.Identity
+{
+    public class Role : IdentityRole
+    {
+        //ПШ сразу определим константу
+        public const string Administrator = "Administrators";
+    }
+}
---
+++ WebStore1p.Domain/Identity/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.AspNetCore.Identity;
+
+
+namespace WebStore1p.Domain.Identity
+{
+    public class User: IdentityUser
+    {
+
+        //ПШ сразу определим константы
+        public const string Administrator = "Administrators";
+        public const string DefaultPassword = "AdminPassword";
+    }
+}
