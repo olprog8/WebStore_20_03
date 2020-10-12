d32ee9d41fad526dc0334bea2bdc5c700c92ec37
olprog8 <solar2010@live.ru>
2020-10-12 18:35:41 +0300

    Настройка системы авторизацим в основном приложении

--- WebStore1p/Startup.cs
+++ WebStore1p/Startup.cs
@@ -15,6 +15,12 @@ using WebStore.DAL.Context;
 
 using WebStore1p.Data;
 
+using Microsoft.AspNetCore.Identity;
+
+using WebStore1p.Domain.Identity;
+
+using System;
+
 
 namespace WebStore1p
 {
@@ -29,7 +35,44 @@ namespace WebStore1p
         {
             //БД ПШ настройка инициализации БД
             services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            
+
+            //L6 Identity Регистрация сервиса Пользователей и Ролей
+            // если быстро то services.AddIdentity<IdentityUser, IdentityRole>();, либо собственные, как ниже
+            services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<WebStoreDB>() //Где хранить данные Identity, но в приложении мб более одного контекста БД, иногда бываает
+                .AddDefaultTokenProviders(); //Регистрируем провайдеров сервисов
+
+            services.Configure<IdentityOptions>(opt =>
+            {
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireLowercase = true;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredUniqueChars = 3;
+
+                //opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxABCD...1234567890";
+                opt.User.RequireUniqueEmail = false;
+
+                opt.Lockout.AllowedForNewUsers = true;
+                opt.Lockout.MaxFailedAccessAttempts = 10;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+            });
+
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.Cookie.Name = "WebStore";
+                opt.Cookie.HttpOnly = true;
+                opt.Cookie.Expiration = TimeSpan.FromDays(10);
+
+                //L6 Identity Пути по которым система будет перенаправлять неавторизованных пользователей на контроллер Account
+                opt.LoginPath = "/Account/Login";
+                opt.LogoutPath = "/Account/Logout";
+                opt.AccessDeniedPath = "/Account/AccessDenied";
+
+                opt.SlidingExpiration = true; //L6 ПШ Параметр который заставит систему автоматически подменять идентификатор сессии пользователя, как он авторизовался
+            });
+
             //БД настройка инициализатора БД
             services.AddTransient<WebStoreDBInitializer>();
 
@@ -64,6 +107,9 @@ namespace WebStore1p
 
             app.UseStaticFiles();
             app.UseDefaultFiles();
+
+            //L6 добавили Identity промежуточное ПО
+            app.UseAuthentication();
 
             //ПШ описание валидации модели с помощью флюент интерфейса
             
