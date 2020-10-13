c8bc03e4d615b1d38790c2d4efdf75b641766ad0
olprog8 <solar2010@live.ru>
2020-10-13 18:38:06 +0300

    Контроллер Account + представления к нему

---
+++ WebStore1p/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore1p.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register() => View();
+
+        public IActionResult Login() => View();
+    }
+}
