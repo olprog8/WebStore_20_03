2d2c748404ab7d9e36ba18860bee2932788c2963
olprog8 <solar2010@live.ru>
2020-10-14 17:32:05 +0300

    Редактируем заголовок для добавления ссылки на форму входа

---
+++ WebStore1p/Components/UserInfoViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore1p.Components
+{
+    public class UserInfoViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
