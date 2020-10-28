b539194536e73f344e5e3135714f6a73796bdd80
olprog8 <solar2010@live.ru>
2020-10-28 17:51:08 +0300

    Список товаров в области администратора

--- WebStore1p/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore1p/Areas/Admin/Controllers/ProductsController.cs
@@ -8,15 +8,15 @@ using Microsoft.AspNetCore.Authorization;
 
 using WebStore1p.Domain.Identity;
 
+using WebStore1p.Infrastructure.Interfaces;
 
 namespace WebStore1p.Areas.Admin.Controllers
 {
     [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class ProductsController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        private readonly IProductData _ProductData; 
+        public ProductsController(IProductData ProductData) => _ProductData = ProductData;
+        public IActionResult Index(/*[FromServices] IProductData Products*/) => View(_ProductData.GetProducts());
     }
 }
