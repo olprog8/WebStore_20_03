6449e0ce8e58db553235ff8e521e92888887d006
olprog8 <solar2010@live.ru>
2020-10-28 18:21:47 +0300

    Заготовки методов редактирования и удаления элементов из каталога товаров

--- WebStore1p/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore1p/Areas/Admin/Controllers/ProductsController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Authorization;
 
 using WebStore1p.Domain.Identity;
 
+using WebStore1p.Domain.Entities;
+
 using WebStore1p.Infrastructure.Interfaces;
 
 namespace WebStore1p.Areas.Admin.Controllers
@@ -18,5 +20,30 @@ namespace WebStore1p.Areas.Admin.Controllers
         private readonly IProductData _ProductData; 
         public ProductsController(IProductData ProductData) => _ProductData = ProductData;
         public IActionResult Index(/*[FromServices] IProductData Products*/) => View(_ProductData.GetProducts());
+
+        public IActionResult Edit(int? id) {
+
+            var product = id is null ? new Product() : _ProductData.GetProductById((int)id);
+
+            if (product is null)
+                return NotFound();
+
+
+            return View(product);
+         
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _ProductData.GetProductById((int)id);
+
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+        };
+
+        [HttpPost, ValidateAntiForgeryToken, ActionName(nameof(Delete))]
+        public IActionResult DeleteConfirm(int id) => RedirectToAction(nameof(Index));
     }
 }
