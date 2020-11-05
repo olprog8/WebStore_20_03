88348e96320f5c901e1cfe333d25f8f47eb68a2b
olprog8 <solar2010@live.ru>
2020-11-05 17:58:22 +0300

    Заготовка профиля пользователя с визуализацией списка заказов

---
+++ WebStore1p/Controllers/UserProfileController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using WebStore1p.Infrastructure.Interfaces;
+
+using WebStore1p.ViewModels.Orders;
+
+namespace WebStore1p.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult Orders([FromServices] IOrderService OrderService) => 
+            View(OrderService.GetUserOrder(User.Identity.Name)
+                .Select(order => new UserOrderViewModel 
+                {
+                    Id = order.Id,
+                    Name = order.Name,
+                    Address = order.Address,
+                    Phone = order.Phone,
+                    TotalSum = order.OrderItems.Sum(i=> i.Price*i.Quantity)
+                }));
+    }
+}
---
+++ WebStore1p/ViewModels/Orders/UserOrderViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore1p.ViewModels.Orders
+{
+    public class UserOrderViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public decimal TotalSum { get; set; }
+
+
+    }
+}
